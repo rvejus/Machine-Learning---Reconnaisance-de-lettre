b3515df845f86deb488eb954641f01ac42b3fa62
rvejus <76257185+rvejus@users.noreply.github.com>
2023-01-14 15:55:06 +0100

    Réajustement de la dll, PMC addapté pour etre utiliser en python

--- dll_machineLearning/fonction.cpp
+++ dll_machineLearning/fonction.cpp
@@ -67,3 +67,184 @@ void AffichageSeparation(float *W , point *test_points , int* test_classes) {
 void HelloWorld() {
 	printf("HelloWorld");
 }
+
+
+
+PMC::PMC(int* npl,int nplSize) {
+	this->D = npl;
+	this->D_size = nplSize;
+	this->L = nplSize - 1;
+
+	// Initialisation des W
+
+	this->W = (float***)malloc(sizeof(float**) * nplSize);
+
+	for (int l = 0; l < nplSize; l++) {
+		this->W[l] = (float**)malloc(sizeof(float*) * (npl[l - 1] + 2));
+		if (l == 0) {
+			//std::cout << "l=0" << std::endl;
+			continue;
+		}
+		for (int i = 0; i <= D[l - 1] + 1; i++) {
+
+			//std::cout << "i" << std::endl;
+			this->W[l][i] = (float*)malloc(sizeof(float) * (npl[l] + 2));
+			for (int j = 1; j <= D[l] + 1; j++) {
+				//std::cout << "j" << std::endl;
+
+				if (j == 0) {
+					this->W[l][i][j] = 0.0;
+				}
+				else {
+					float random = (float)rand() / RAND_MAX * 2 - 1;
+					this->W[l][i][j] =random;
+				}
+			}
+		}
+	}
+	std::cout << "finished W" << std::endl;
+	//Initialisation des X et des deltas
+	//Init de la mémoire de la 1ere dimention pour les tableaux
+	this->X = (float**)malloc(sizeof(float*) * nplSize);
+	this->delta = (float**)malloc(sizeof(float*) * nplSize);
+	for (int l = 0; l < nplSize; l++) {
+		//std::cout << "l" << std::endl;
+		X[l] = (float*)malloc(sizeof(float) * (npl[l] + 2));
+		delta[l] = (float*)malloc(sizeof(float) * (npl[l] + 2));
+		for (int j = 0; j <= D[l] + 1; j++) {
+			//std::cout << "j" << std::endl;
+			delta[l][j] = 0;
+			if (j == 0) {
+				X[l][j] =1;
+			}
+			else {
+				X[l][j] = 0;
+			}
+		}
+	}
+
+	std::cout << "finished X & delta" << std::endl;
+}
+
+PMC::~PMC() {
+	// Desaloc de W
+	free(this->W);
+	
+	free(this->X);
+	
+	free(this->delta);
+
+	free(this->D);
+}
+
+void PMC::_propagate(float *inputs, bool is_classification) {
+	for (int j = 1; j <= D[0] + 1; j++) {
+		X[0][j] = inputs[j - 1];
+	}
+	//std::cout << "finished first for" << std::endl;
+
+
+	for (int l = 1; l < D_size; l++) {
+		//std::cout << "l" << std::endl;
+
+		for (int j = 1; j < D[l] + 1; j++) {
+			//std::cout << "j" << std::endl;
+			int total = 0;
+			for (int i = 0; i <= D[l - 1] + 1; i++) {
+				//std::cout <<"l= " << l << std::endl;
+				//std::cout << "i= " << i << std::endl;
+				//std::cout << "j= " << j << std::endl;
+				//std::cout << "W[l][0][0]= " << W[l][0][0] << std::endl;
+				//std::cout << "W[l][i][0]= " << W[l][i][0] << std::endl;
+				//std::cout << "D[l - 1] + 1= " << D[l - 1] + 1 << std::endl;
+				//std::cout << "X[l - 1][i]= " << X[l - 1][i] << std::endl;
+				total += this->W[l][i][j] * X[l - 1][i];
+			}
+			//std::cout << "finished i for" << std::endl;
+			X[l][j] = total;
+			if (is_classification || l < L) {
+				//std::cout << "is_class" << std::endl;
+				X[l][j] = std::tanh(total);
+				//std::cout << "is_class passed" << std::endl;
+			}
+			//std::cout << "end j for" << std::endl;
+		}
+	}
+	//std::cout << "finished propagate" << std::endl;
+}
+
+float* PMC::predict(float* inputs, bool is_classification) {
+	this->_propagate(inputs, is_classification);
+	//D[L]+1 pour la taille du dernier Layer et -1 pour retirer le biais 
+	int output_size = D[L] + 1 - 1;
+	float* output = (float*)malloc(output_size * sizeof(float));
+	for (int i = 0; i < output_size; i++) {
+		output[i] = X[L][i + 1];
+	}
+	return output;
+}
+
+void PMC::train(float** X_train,
+	int X_train_size,
+	float** Y_train,
+	int Y_train_size,
+	bool is_classification,
+	float alpha = 0.01,
+	int nb_iter = 10000)
+{
+	//std::cout << "train" << std::endl;
+	for (int it = 0; it <= nb_iter; it++) {
+		//std::cout << "it= " << it << std::endl;
+		int k = rand() % (X_train_size);
+		//std::cout << "k= " << k << std::endl;
+		float* Xk = X_train[k];
+		//std::cout << "Xk.size()= " << Xk.size() << std::endl;
+		float* Yk = Y_train[k];
+		//std::cout << "Yk.size()= " << Yk.size() << std::endl;
+
+		this->_propagate(Xk, is_classification);
+
+		//std::cout << "D len " << D.size() << std::endl;
+		for (int j = 1; j < D[L] + 1; j++) {
+			//std::cout << "j= " << j << std::endl;
+
+			delta[L][j] = X[L][j] - Yk[j - 1];
+			if (is_classification) {
+				//std::cout << "is_class" << std::endl;
+				delta[L][j] = delta[L][j] * (std::pow(1 - X[L][j], 2));
+				//std::cout << "is_class finished" << std::endl;
+			}
+		}
+		//std::cout << "first for finished" << std::endl;
+
+		for (int l = D_size - 1; l >= 2; l--) {
+			//std::cout << "l= "<<l << std::endl;
+			for (int i = 1; i < D[l - 1] + 1; i++) {
+				//std::cout << "i= " << i << std::endl;
+				float total = 0;
+				for (int j = 1; j < D[l - 1] + 1; j++) {
+					//std::cout << "gonna calc total " << std::endl;
+					//std::cout << "delta[l][0]= " << delta[l][0] << std::endl;
+					//std::cout << "W[l][i][j]= " << this->W[l][i][j] <<std::endl;
+					total += this->W[l][i][j] * delta[l][j];
+					//std::cout << "total calculated " << i << std::endl;
+				}
+				delta[l - 1][i] = (std::pow(1 - X[l - 1][i], 2)) * total;
+			}
+		}
+		//std::cout << "second for finished" << std::endl;
+		for (int l = 1; l < D_size; l++) {
+			//std::cout << "D.size()= " << D.size() << std::endl;
+			//std::cout << "l= " << l << std::endl;
+			for (int i = 0; i < D[l - 1] + 1; i++) {
+				//std::cout << "i= " << i << std::endl;
+				for (int j = 1; j < D[l] + 1; j++) {
+					//std::cout << "j= " << j << std::endl;
+					//std::cout << "delta[l][j]= " << delta[l][j] << std::endl;
+					//std::cout << "W[l][i][j]= " << this->W[l][i][j] << std::endl;
+					this->W[l][i][j] += -alpha * X[l - 1][i] * delta[l][j];
+				}
+			}
+		}
+	}
+}
--- dll_machineLearning/fonction.h
+++ dll_machineLearning/fonction.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 
@@ -25,3 +26,26 @@ extern "C"	FONCTION_API void HelloWorld();
 extern "C"	FONCTION_API void EntrainementLineaire(point *points, int *classes, float *W, int nbElem);
 extern "C"	FONCTION_API float RandomFloat(float min, float max);
 extern "C"	FONCTION_API void AffichageSeparation(float* W, point * test_points, int* test_classes);
+
+
+class __declspec(dllexport) PMC
+{
+public:
+	PMC(int *npl,int nplSize);
+	~PMC();
+	// Liste des poids
+	float*** W;
+	//le nombre de neurones appartenant à la couche l 
+	int *D;
+
+	int D_size;
+	//identifiant de la derniere couche 
+	int L;
+	//Valeurs de sortie des Neurones
+	float** X;
+	float** delta;
+
+	void _propagate(float* inputs, bool is_classification);
+	float *predict(float* inputs, bool is_classification);
+	void train(float** X_train, int X_train_size, float** Y_train, int Y_train_size, bool is_classification, float alpha, int nb_iter);
+};
